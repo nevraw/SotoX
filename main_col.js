4d0ca073a7a9b406afeea569abf27f8c46a8c8d7
nevraw <nevraw@users.noreply.github.com>
2015-11-04 19:36:48 +0000

    Update main_col.js

--- main_col.js
+++ main_col.js
@@ -23,62 +23,45 @@ function buttonHandler() {
  });
 }
 
-// Radio control for time display
-var $digitalValue;
-$("input[name=digital]").change(function () {
- $digitalValue = parseInt(this.value);
-});
-
-var $dateValue;
-$("input[name=datemode]").change(function () {
- $datemodeValue = parseInt(this.value);
-});
-
 function loadOptions() {
- if (localStorage.digital) {
-  $digitalValue = localStorage.digital;
-//  console.log('localStorage.digital: ' + $digitalValue);
-  // setting radio' value
- } else {
-  $digitalValue = 1;
-//  console.log('localStorage.digital was undefined, now set to: ' + $digitalValue);
- }
- $("input[name=digital][value='" + $digitalValue + "']").attr('checked', 'checked');
 
- var $numColorPicker = $('#numColorPicker');
-
- if (localStorage.numColor) {
-  $numColorPicker[0].value = localStorage.numColor;
+ var $bgColorPicker = $('#bgColorPicker');
+ if (localStorage.bgColor) {
+  $bgColorPicker[0].value = localStorage.bgColor;
  }
  
- if (localStorage.datemode) {
-  $datemodeValue = localStorage.datemode;
-//  console.log('localStorage.datemode: ' + $datemodeValue);
-  // setting radio' value
- } else {
-  $datemodeValue = 0;
-//  console.log('localStorage.datemode was undefined, now set to: ' + $datemodeValue);
+ var $minColorPicker = $('#minColorPicker');
+ if (localStorage.minColor) {
+  $minColorPicker[0].value = localStorage.minColor;
  }
- $("input[name=datemode][value='" + $datemodeValue + "']").attr('checked', 'checked');
+ 
+ var $bgColorPicker = $('#hourColorPicker');
 
+ if (localStorage.hourColor) {
+  $hourColorPicker[0].value = localStorage.hourColor;
+ }
+  
+ 
+ 
+ 
 } 
 
 function getAndStoreConfigData() {
- var $numColorPicker = $('#numColorPicker');
-
- console.log('digital value: ' + $digitalValue);
+ var $bgColorPicker = $('#bgColorPicker');
+ var $minColorPicker = $('#minColorPicker');
+ var $hourColorPicker = $('#hourColorPicker');
 
  var options = {
-  digital: $digitalValue,
-  numColor: $numColorPicker.val(),
-  datemode: $datemodeValue
+  bgColor: $bgColorPicker.val(),
+  minColor: $minColorPicker.val(),
+  hourColor: $hourColorPicker.val()
  };
  
  console.log('Got options: ' + JSON.stringify(options));
 
- localStorage.digital = $digitalValue;
- localStorage.numColor = options.numColor;
- localStorage.datemode = $datemodeValue;
+ localStorage.bgcolor   = options.bgColor;
+ localStorage.minColor  = options.minColor;
+ localStorage.hourColor = options.hourColor;
 
  return options;
 }
